5b8071f77384979394498059c4ffec9cbbf3fedd
Эдуард <eduard.poyda@gmail.com>
2014-11-28 21:10:51 +0300

    Test
    откат

--- REALTASKPROJECTWITHAHARDTESTFORGITHUBSYSTEMWITHNOLICENCEFORPURESTUDENTS/File1.cpp
+++ REALTASKPROJECTWITHAHARDTESTFORGITHUBSYSTEMWITHNOLICENCEFORPURESTUDENTS/File1.cpp
@@ -3,10 +3,16 @@
 
 #include <tchar.h>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+#include <stdio.h>
+>>>>>>> parent of 77ecb43... 1
 #include <iostream>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+<<<<<<< HEAD
 	std::cout << "GitHub horosho, no SourceTree luchshe!\n";
 	system("pause");
 =======
@@ -16,4 +22,8 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 >>>>>>> parent of d89681b... Hello world test
 	return 0;
+=======
+	cout << "Hello, world!" << endl;
+    	return 0; 
+>>>>>>> parent of 77ecb43... 1
 }
