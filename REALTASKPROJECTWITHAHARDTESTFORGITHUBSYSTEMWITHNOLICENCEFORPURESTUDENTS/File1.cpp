26b8934c7e34615226e648613a1da56b79418bdd
Эдуард <eduard.poyda@gmail.com>
2014-11-28 21:06:07 +0300

    Revert "Hello world test"
    This reverts commit d89681badd95d85b3a7c981f2c52894c5d4cff66.
    
    Conflicts:
    
    	REALTASKPROJECTWITHAHARDTESTFORGITHUBSYSTEMWITHNOLICENCEFORPURESTUDENTS/File1.cpp

--- REALTASKPROJECTWITHAHARDTESTFORGITHUBSYSTEMWITHNOLICENCEFORPURESTUDENTS/File1.cpp
+++ REALTASKPROJECTWITHAHARDTESTFORGITHUBSYSTEMWITHNOLICENCEFORPURESTUDENTS/File1.cpp
@@ -2,11 +2,18 @@
 #pragma argsused
 
 #include <tchar.h>
+<<<<<<< HEAD
 #include <iostream>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout << "GitHub horosho, no SourceTree luchshe!\n";
 	system("pause");
+=======
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+>>>>>>> parent of d89681b... Hello world test
 	return 0;
 }
